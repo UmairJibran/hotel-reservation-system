3900254ad5dac4f37301011b40e292dcf9475ab2
Umair Jibran <umairjibran7@gmail.com>
2020-07-01 14:16:36 +0500

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,28 @@
             <h1 class="center">Hotel Management System</h1>
         </div>
         <form method="POST">
-            <select name="city">
-                <option value=""></option>
-            </select>
+            <div class="form-group">
+                <label for="city">Select a City</label>
+                <select name="city" id="city" class="form-control form-control-lg" required>
+                    <option selected disabled value="">Select a City</option>
+                    <?php
+                        $sql = "SELECT `city_NAME` FROM `tbl-cities`;";
+                        $result = $conn->query($sql);
+                        $rows = $result->num_rows;
+                        if($rows >= 1)
+                            while($data = $result->fetch_assoc()){
+                                $cityName = $data['city_NAME'];
+                                echo "<option value='${cityName}'>${cityName}</option>";
+                            }
+                    ?>
+                </select>
+            </div>
+            <input type="submit" value="Show Hotels" name="show" class="btn btn-primary">
         </form>
     </div>
 </body>
 </html>
+
+<?php
+    if(isset($_POST['show'])) header("location:hotels.php?city=".$_POST['city']);
+?>
