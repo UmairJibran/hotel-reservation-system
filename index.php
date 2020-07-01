c0106121eb877da67350bdc272ba7c802e988908
Umair Jibran <umairjibran7@gmail.com>
2020-07-01 13:56:31 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <div class="container container-fluid">
+        <div class="jumbotron">
+            <h1 class="center">Hotel Management System</h1>
+        </div>
+        <form method="POST">
+            <select name="city">
+                <option value=""></option>
+            </select>
+        </form>
+    </div>
 </body>
 </html>
