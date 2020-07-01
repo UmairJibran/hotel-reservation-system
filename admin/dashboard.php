f4c3923eb8d7fe81668001ee9c2f9e6bdee99573
Umair Jibran <umairjibran7@gmail.com>
2020-07-01 18:34:11 +0500

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,7 +19,6 @@
     <div class="container container-fluid">
         <ul>
             <br><li><a href="./addHotel.php" class="list-group-item list-group-item-action">Add A Hotel</a></li>
-            <br><li><a href="./updateHotel.php" class="list-group-item list-group-item-action">Update a Hotel</a></li>
             <br><li><a href="./addCity.php" class="list-group-item list-group-item-action">Add a City</a></li>
             <br><li><a href="./updateUser.php" class="list-group-item list-group-item-action">Update an existing User</a></li>
         </ul>
