9139bce15bcf04590271246c5297ac748bb9662e
Umair Jibran <umairjibran7@gmail.com>
2020-07-01 14:59:02 +0500

    Create hotel-detail.php

---
+++ hotel-detail.php
@@ -0,0 +1,27 @@
+<?php
+    require_once('./server/connection.php');
+    $hotel_id = $_GET['hotel'];
+    $sql = "SELECT `hotel_NAME`,`hotel_CITY` FROM `tbl-hotel-info` WHERE `hotel_ID` = '${hotel_id}'";
+    $result = $conn->query($sql);
+    $hotel = $result->fetch_assoc();
+    $hotel_name = $hotel['hotel_NAME'];
+    $hotel_city = $hotel['hotel_CITY'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $hotel_name . "," . $hotel_city?></title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container container-fluid">
+        <div class="jumbotron">
+            <h3 class="center">Welcome to <?php echo $hotel_name . ", " . $hotel_city ?></h3>
+        </div>
+    </div>
+</body>
+</html>
