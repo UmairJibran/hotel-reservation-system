0bc66d09546aa1fa7ea96e0afb3663a3158cbd97
Umair Jibran <umairjibran7@gmail.com>
2020-07-01 17:48:04 +0500

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,3 +2,26 @@
     session_start();
     if(!isset($_SESSION['id'])) header('location:./login.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="jumbotron center">
+        <h3>Select an Option to Perform</h3>
+    </div>
+    <div class="container container-fluid">
+        <ul>
+            <br><li><a href="#" class="list-group-item list-group-item-action">Add A Hotel</a></li>
+            <br><li><a href="#" class="list-group-item list-group-item-action">Update a Hotel</a></li>
+            <br><li><a href="#" class="list-group-item list-group-item-action">Update an existing User</a></li>
+        </ul>
+    </div>
+</body>
+</html>
