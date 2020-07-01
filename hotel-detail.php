b9c627ae5f10f378243922e52c68ffa127a226ed
Umair Jibran <umairjibran7@gmail.com>
2020-07-01 16:27:41 +0500

    Update hotel-detail.php

--- hotel-detail.php
+++ hotel-detail.php
@@ -1,11 +1,24 @@
 <?php
     require_once('./server/connection.php');
     $hotel_id = $_GET['hotel'];
-    $sql = "SELECT `hotel_NAME`,`hotel_CITY` FROM `tbl-hotel-info` WHERE `hotel_ID` = '${hotel_id}'";
+    $sql = "SELECT * FROM `tbl-hotel-info` WHERE `hotel_ID` = '${hotel_id}'";
     $result = $conn->query($sql);
     $hotel = $result->fetch_assoc();
-    $hotel_name = $hotel['hotel_NAME'];
-    $hotel_city = $hotel['hotel_CITY'];
+    $name = $hotel['hotel_NAME'];
+    $city = $hotel['hotel_CITY'];
+    $street = $hotel['hotel_STREET'];
+    $building = $hotel['hotel_BUILDING'];
+    $email = $hotel['hotel_EMAIL'];
+    $contact = $hotel['hotel_CONTACT'];
+    $website = $hotel['hotel_WEBSITE'];
+    $n1R = $hotel['hotel_NSINGLEROOMS'];
+    $n2R = $hotel['hotel_NDOUBLEROOMS'];
+    $n3R = $hotel['hotel_NTRIPLEROOMS'];
+    $n4R = $hotel['hotel_NFOURPROOMS'];
+    $n1BR = $hotel['hotel_BOOKED_SINGLE'];
+    $n2BR = $hotel['hotel_BOOKED_DOUBLE'];
+    $n3BR = $hotel['hotel_BOOKED_TRIPLE'];
+    $n4BR = $hotel['hotel_BOOKED_FOUR'];
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +26,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $hotel_name . ", " . $hotel_city?></title>
+    <title><?php echo $name . ", " . $city?></title>
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -21,9 +34,40 @@
     <a href="./"><button class="go-home">Go Home</button></a>
     <div class="container container-fluid">
         <div class="jumbotron">
-            <h3 class="center">Welcome to <?php echo $hotel_name . ", " . $hotel_city ?></h3>
+            <h3 class="center">Welcome to <?php echo $name . ", " . $city ?></h3>
         </div>
-
+        <section class='row'>
+            <h2 class='center col col-3 text-primary'>Name</h2>
+            <h4 class='center col col-3 text-primary'><?php echo $name?></h4>
+            <h2 class='center col col-3 text-primary'>City</h2>
+            <h4 class='center col col-3 text-primary'><?php echo $city?></h4>
+        </section>
+        <br>
+        <section class="row center">
+            <strong class="col col-1">Road/Street</strong>
+            <p class="col col-2 text-info"><?php echo $street?></p>
+            <strong class="col col-1">Building</strong>
+            <p class="col col-2 text-info"><?php echo $building?></p>
+            <p class="text-info col col-2">Send <a href="mailto:<?php echo $email?>" class="text-info"><strong>Email</strong></a></p>
+            <strong class="col col-1">Contact</strong>
+            <p class="col col-2 text-info"><?php echo $contact?></p>
+            <a href="http://<?php echo $website?>">Their Site</a>
+        </section>
+        <p class="text-monospace text-lg-center">Total Rooms:<?php $total = $n1R + $n2R + $n3R + $n4R; $free =  $total - ($n1BR + $n2BR + $n3BR + $n4BR); echo "<font color='green'><strong>${free}</strong></font> free rooms out of ${total}";?></p>
+        <section class="row">
+            <span class="col col-2"><strong>Single Bed Room(s)</strong></span>
+            <span class="col col-3"><?php $free = $n1R - $n1BR;echo "<font color='green'><strong>${free}</strong></font> free rooms out of " . $n1R; ?></span>
+            <span class="col col-2"></span>
+            <span class="col col-2"><strong>Double Bed Room(s)</strong></span>
+            <span class="col col-3"><?php $free = $n2R - $n2BR;echo "<font color='green'><strong>${free}</strong></font> free rooms out of " . $n2R; ?></span>
+        </section>
+        <section class="row">
+            <span class="col col-2"><strong>Triple Bed Room(s)</strong></span>
+            <span class="col col-3"><?php $free = $n3R - $n3BR;echo "<font color='green'><strong>${free}</strong></font> free rooms out of " . $n3R; ?></span>
+            <span class="col col-2"></span>
+            <span class="col col-2"><strong>Four Bed Room(s)</strong></span>
+            <span class="col col-3"><?php $free = $n4R - $n4BR;echo "<font color='green'><strong>${free}</strong></font> free rooms out of " . $n4R; ?></span>
+        </section>
     </div>
 </body>
 </html>
