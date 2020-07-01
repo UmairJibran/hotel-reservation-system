6cd68ab219ab1c7bcc72d5e1014f5a33bd740bfe
Umair Jibran <umairjibran7@gmail.com>
2020-07-01 19:18:02 +0500

    Update reserve.php

--- reserve.php
+++ reserve.php
@@ -2,6 +2,27 @@
     require_once("./server/connection.php");
     $hotel_id = $_GET['hotel'];
     $customer_cp_id = $_GET['id'];
+    $first_name = '';
+    $last_name = '';
+    $email = '';
+    $contact = '';
+    $credit = '';
+    $customer_id = 0;
+    $exists = false;
+    $sql = "SELECT * FROM `tbl-customer` WHERE `customer_identity_number` = '${customer_cp_id}'";
+    $result = $conn->query($sql);
+    $rows = $result->num_rows;
+    if($rows == 1){
+        $exists = true;
+        while($userData = $result->fetch_assoc()){
+            $customer_id = $userData['customer_id'];
+            $first_name = $userData['customer_first_name'];
+            $last_name = $userData['customer_last_name'];
+            $email = $userData['customer_email'];
+            $contact = $userData['customer_contact'];
+            $credit = $userData['customer_card'];
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -30,31 +51,62 @@
             <label>Your CNIC/Passport Number</label>
             <div class="input-group">
                 <input type="text" class="form-control" readonly value='<?php echo $customer_cp_id?>'>
-                &nbsp;&nbsp;
-                <select class='form-control' name="room">
+                &emsp;
+                <select class='form-control' required name="room">
                     <option value="" selected disabled>Select a Room</option>
+                    <option value="s">Single Bed</option>
+                    <option value="m">Double Bed</option>
+                    <option value="l">Triple Bed</option>
+                    <option value="xl">Four Bed</option>
                 </select>
             </div>
             <br>
+            <div class="input-group">
+                    <label for="start">Start Date</label>
+                    &emsp;
+                    <input required type="date" name="start" id="start" class="form-control">
+                    &emsp;
+                    <label for="end">End Date</label>
+                    &emsp;
+                    <input required type="date" name="end" id="end" class="form-control">
+                </div>
+            <br>
             <label>Your Name</label>
             <div class="input-group">
-                <input required type="text" class="form-control" placeholder="First Name" name='fname'>
-                &nbsp;
-                <input required type="text" class="form-control" placeholder="Last Name" name='lname'>
+                <input required type="text" class="form-control" value='<?php echo $first_name?>' placeholder="First Name" name='fname'>
+                &emsp;
+                <input required type="text" class="form-control" value='<?php echo $last_name?>' placeholder="Last Name" name='lname'>
             </div>
             <br>            
             <label>Your Necassary Data</label>
             <div class="input-group">
-                <input required type="email" class="form-control" placeholder="Contact Email" name='email'>
-                &nbsp;
-                <input required type="number" class="form-control" placeholder="Contact Number" name='contact'>
-                &nbsp;
-                <input required type="number" class="form-control" placeholder="Credit Card" name='ccard'>
+                <input required type="email" class="form-control" value='<?php echo $email?>' placeholder="Contact Email" name='email'>
+                &emsp;
+                <input required type="number" class="form-control" value='<?php echo $contact?>' placeholder="Contact Number" name='contact'>
+                &emsp;
+                <input required type="number" class="form-control" value='<?php echo $credit?>' placeholder="Credit Card" name='ccard'>
             </div>
             <br>
             <small>All the fields are required</small>
-            <input type="submit" value="Book" class='btn btn-outline-primary right'>
+            <input type="submit" name='book' value="Book" class='btn btn-outline-primary right'>
         </form>
     </div>
 </body>
 </html>
+
+<?php
+if(isset($_POST['book'])){
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+    $credit = $_POST['ccard'];
+    if($exists)
+        $sql = "UPDATE `tbl-customer` SET `customer_first_name` = '${first_name}',`customer_last_name` = '${last_name}',`customer_email` = '${email}',`customer_contact` = '${contact}',`customer_card`='${customer_card}' WHERE `tbl-customer`.`customer_id` = ${customer_id};";
+    else
+        $sql = "INSERT INTO `tbl-customer` (`customer_id`, `customer_first_name`, `customer_last_name`, `customer_identity_number`, `customer_contact`, `customer_type`, `customer_email`, `customer_card`) VALUES (NULL, '${first_name}', '${last_name}', '${customer_cp_id}', '${contact}', \'Normal\', '${email}', '${credit}')";
+    if($conn->query($sql)===true){
+        header('location:./');
+    }
+}
+?>
